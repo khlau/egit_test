d49282dee3f101cbdf8b121c91276ff548d1a029
khlau <khlau@wdcigroup.net>
2011-05-24 14:32:23 +0800

    egit_test

---
+++ src/hello_egit.java
@@ -0,0 +1,12 @@
+
+public class hello_egit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello EGit");
+	}
+
+}
